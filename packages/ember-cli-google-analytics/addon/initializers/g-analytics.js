1a5b8b3c58e624676a6d8e315fcd8372c71ab2b4
Roberto Rodriguez <manufacturedba@gmail.com>
2023-09-18 01:31:47 -0700

    Update g-analytics.js
    Test removal of implicit injections

--- packages/ember-cli-google-analytics/addon/initializers/g-analytics.js
+++ packages/ember-cli-google-analytics/addon/initializers/g-analytics.js
@@ -1,16 +1,16 @@
 import { get } from '@ember/object';
 
 export function initialize(app) {
-  const ENV = app.resolveRegistration('config:environment');
-  const analytics = get(ENV, 'ember-cli-google.analytics');
+  // const ENV = app.resolveRegistration('config:environment');
+  // const analytics = get(ENV, 'ember-cli-google.analytics');
 
-  if (analytics.version === 'v4') {
-    app.inject('route', 'gtag', 'service:gtag');
-    app.inject('controller', 'gtag', 'service:gtag');
-  } else {
-    app.inject('route', 'analytics', 'service:g-analytics');
-    app.inject('controller', 'analytics', 'service:g-analytics');
-  }
+  // if (analytics.version === 'v4') {
+  //   app.inject('route', 'gtag', 'service:gtag');
+  //   app.inject('controller', 'gtag', 'service:gtag');
+  // } else {
+  //   app.inject('route', 'analytics', 'service:g-analytics');
+  //   app.inject('controller', 'analytics', 'service:g-analytics');
+  // }
 }
 
 export default {
